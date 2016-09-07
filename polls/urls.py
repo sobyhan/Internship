f5bef92d6311887e32ee802f71cdf5dc04c11b9b
sobyhan <sobyhan@hotmail.com>
2016-09-07 16:08:08 +0430

    New Update

--- polls/urls.py
+++ polls/urls.py
@@ -2,5 +2,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url (r'^$', views.index, name = 'index')
+    url (r'^$', views.index, name = 'index'),
+    url (r'^(?P<question_id>[0-9]+)/$', views.details, name='details'),
+    url (r'^(?P<question_id>[0-9]+)/results/$', views.result, name='results'),
+    url (r'^(?P<question_id>[0-9]+)/votes/$', views.vote, name='votes'),
+
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
 # Create your views here.
 
 def index (request):
-    return HttpResponse ("Hello World, This is first Page of Polls App :D")
+    lastest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in lastest_question_list])
+    return HttpResponse(output)
+
+def details (request, question_id):
+    return HttpResponse ("Now You're looking to question No. %s." % question_id)
+
+def result (request, question_id):
+    response = "You're looing at the result of question %s."
+    return HttpResponse (response % question_id)
+
+def vote (request, question_id):
+    return HttpResponse ("You're voting of question %s." % question_id)
+
+
+
